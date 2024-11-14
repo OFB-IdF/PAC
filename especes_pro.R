aaedb19dc8f3b575900b68e0e5c76aa3f8e36949
cedric.mondy <cedric.mondy@ofb.gouv.fr>
2024-11-14 14:59:28 +0100

    premiere version

---
+++ especes_pro.R
@@ -0,0 +1,259 @@
+protections <- list(
+  `Arrêté du 8 janvier 2021 fixant la liste des amphibiens et des reptiles représentés sur le territoire métropolitain protégés sur l'ensemble du territoire national et les modalités de leur protection : Article 2` = list(
+    I = "Sont interdits, sur tout le territoire métropolitain et en tout temps :
+      - la destruction ou l'enlèvement des œufs et des nids, la destruction, la mutilation, la capture ou l'enlèvement des animaux ;
+    - la perturbation intentionnelle des animaux, pour autant que la perturbation remette en cause le bon accomplissement des cycles biologiques de l'espèce considérée.",
+    II = "Sont interdites sur les parties du territoire métropolitain où l'espèce est présente ainsi que dans l'aire de déplacement naturel des noyaux de populations existants, la destruction, l'altération ou la dégradation des sites de reproduction et des aires de repos des animaux. Ces interdictions s'appliquent aux éléments physiques ou biologiques réputés nécessaires à la reproduction ou au repos de l'espèce considérée, aussi longtemps qu'ils sont effectivement utilisés ou utilisables au cours des cycles successifs de reproduction ou de repos de cette espèce et pour autant que la destruction, l'altération ou la dégradation remette en cause le bon accomplissement de ces cycles biologiques.",
+    III = "Sont interdits, sur tout le territoire national et en tout temps, la détention, le transport, la naturalisation, le colportage, la mise en vente, la vente ou l'achat, l'utilisation, commerciale ou non, des spécimens prélevés :
+      - dans le milieu naturel du territoire métropolitain de la France, après le 12 mai 1979 ;
+    - dans le milieu naturel du territoire européen des autres Etats membres de l'Union européenne, après la date d'entrée en vigueur de la directive du 21 mai 1992 susvisée."
+  ),
+  `Liste des mollusques protégés sur l'ensemble du territoire français métropolitain : Article 2` = list(I = "Sont interdits sur tout le territoire métropolitain et en tout temps la destruction ou l'enlèvement des oeufs, la destruction, la mutilation, la capture ou l'enlèvement, la perturbation intentionnelle des animaux dans le milieu naturel.", II = "Sont interdites sur les parties du territoire métropolitain où l'espèce est présente ainsi que dans l'aire de déplacement naturel des noyaux de populations existants la destruction, l'altération ou la dégradation des sites de reproduction et des aires de repos des animaux. Ces interdictions s'appliquent aux éléments physiques ou biologiques réputés nécessaires à la reproduction ou au repos de l'espèce considérée, aussi longtemps qu'ils sont effectivement utilisés ou utilisables au cours des cycles successifs de reproduction ou de repos de cette espèce et pour autant que la destruction, l'altération ou la dégradation remette en cause le bon accomplissement de ces cycles biologiques.", III = "Sont interdits sur tout le territoire national et en tout temps la détention, le transport, la naturalisation, le colportage, la mise en vente, la vente ou l'achat, l'utilisation, commerciale ou non, des spécimens prélevés :- dans le milieu naturel du territoire métropolitain de la France, après le 24 novembre 1992 ;- dans le milieu naturel du territoire européen des autres Etats membres de l'Union européenne, après la date d'entrée en vigueur de la directive du 21 mai 1992 susvisée."
+  ),
+  `Liste des espèces de poissons protégées sur l'ensemble du territoire français national : Article 1` = list(
+    I = "Sont interdits en tout temps, sur tout le territoire national : La destruction ou l'enlèvement des oeufs",
+    II = "Sont interdits en tout temps, sur tout le territoire national : La destruction, l'altération ou la dégradation des milieux particuliers, et notamment des lieux de reproduction, désignés par arrêté préfectoral"
+  ),
+  `Liste des insectes protégés sur l'ensemble du territoire et les modalités de leur protection : Article 2` = list(
+    I = "Sont interdits, sur tout le territoire métropolitain et en tout temps, la destruction ou l'enlèvement des oeufs, des larves et des nymphes, la destruction, la mutilation, la capture ou l'enlèvement, la perturbation intentionnelle des animaux dans le milieu naturel.",
+    II = "Sont interdites, sur les parties du territoire métropolitain où l'espèce est présente ainsi que dans l'aire de déplacement naturel des noyaux de populations existants la destruction, l'altération ou la dégradation des sites de reproduction et des aires de repos des animaux. Ces interdictions s'appliquent aux éléments physiques ou biologiques réputés nécessaires à la reproduction ou au repos de l'espèce considérée, aussi longtemps qu'ils sont effectivement utilisés ou utilisables au cours des cycles successifs de reproduction ou de repos de cette espèce et pour autant que la destruction, l'altération ou la dégradation remette en cause le bon accomplissement de ces cycles biologiques.",
+    III = "Sont interdits, sur tout le territoire national et en tout temps, la détention, le transport, la naturalisation, le colportage, la mise en vente, la vente ou l'achat, l'utilisation commerciale ou non, des spécimens prélevés :
+	- dans le milieu naturel du territoire métropolitain de la France, après le 24 septembre 1993 ;
+	- dans le milieu naturel du territoire européen des autres Etats membres de l'Union européenne, après la date d'entrée en vigueur de la directive du 21 mai 1992 susvisée."
+  ),
+  `Liste des mammifères terrestres protégés sur l'ensemble du territoire français et les modalités de leur protection : Article 2` = list(
+    I = "Sont interdits sur tout le territoire métropolitain et en tout temps la destruction, la mutilation, la capture ou l'enlèvement, la perturbation intentionnelle des animaux dans le milieu naturel.",
+    II =  "Sont interdites sur les parties du territoire métropolitain où l'espèce est présente, ainsi que dans l'aire de déplacement naturel des noyaux de populations existants, la destruction, l'altération ou la dégradation des sites de reproduction et des aires de repos des animaux. Ces interdictions s'appliquent aux éléments physiques ou biologiques réputés nécessaires à la reproduction ou au repos de l'espèce considérée, aussi longtemps qu'ils sont effectivement utilisés ou utilisables au cours des cycles successifs de reproduction ou de repos de cette espèce et pour autant que la destruction, l'altération ou la dégradation remette en cause le bon accomplissement de ces cycles biologiques."
+  ),
+  `Liste des oiseaux protégés sur l'ensemble du territoire et les modalités de leur protection : Article 3` = list(
+    I = "Sont interdits sur tout le territoire métropolitain et en tout temps :
+      ― la destruction intentionnelle ou l'enlèvement des œufs et des nids ;
+― la destruction, la mutilation intentionnelles, la capture ou l'enlèvement des oiseaux dans le milieu naturel ;
+    ― la perturbation intentionnelle des oiseaux, notamment pendant la période de reproduction et de dépendance, pour autant que la perturbation remette en cause le bon accomplissement des cycles biologiques de l'espèce considérée.",
+    II = "Sont interdites sur les parties du territoire métropolitain où l'espèce est présente ainsi que dans l'aire de déplacement naturel des noyaux de populations existants la destruction, l'altération ou la dégradation des sites de reproduction et des aires de repos des animaux. Ces interdictions s'appliquent aux éléments physiques ou biologiques réputés nécessaires à la reproduction ou au repos de l'espèce considérée, aussi longtemps qu'ils sont effectivement utilisés ou utilisables au cours des cycles successifs de reproduction ou de repos de cette espèce et pour autant que la destruction, l'altération ou la dégradation remette en cause le bon accomplissement de ces cycles biologiques.",
+    III = "Sont interdits sur tout le territoire national et en tout temps la détention, le transport, la naturalisation, le colportage, la mise en vente, la vente ou l'achat, l'utilisation commerciale ou non des spécimens d'oiseaux prélevés :
+― dans le milieu naturel du territoire métropolitain de la France, après la date d'entrée en vigueur de l'interdiction de capture ou d'enlèvement concernant l'espèce à laquelle ils appartiennent ;
+― dans le milieu naturel du territoire européen des autres Etats membres de l'Union européenne, après la date d'entrée en vigueur dans ces Etats de la directive du 2 avril 1979 susvisée."
+  ),
+  `Protection des écrevisses autochtones sur le territoire français métropolitain : Article 1` = "Il est interdit d'altérer et de dégrader sciemment les milieux particuliers aux espèces"
+)
+
+limites_idf <- sf::st_read("C:/QGIS-CUSTOM/DATA/VECTEUR/administration/ADMIN EXPRESS/admin_express.gpkg", layer = "departements") |>
+  dplyr::filter(INSEE_REG == 11)
+
+sp_pro <- sf::st_read("C:/QGIS-CUSTOM/DATA/VECTEUR/especes/sp_pro.gpkg", layer = "observations")
+
+liste_sp_pro <- sp_pro  |>
+  sf::st_drop_geometry() |>
+  dplyr::count(code_ref, CLASSE, ORDRE, FAMILLE, ESPECE) |>
+  dplyr::arrange(CLASSE, ORDRE, FAMILLE, ESPECE) |>
+  tibble::tibble() |>
+  dplyr::mutate(
+    NOM = purrr::map(
+      code_ref,
+      .f = function(x) {
+        taxref4R::get_taxa(x)$frenchVernacularName
+        },
+      .progress = TRUE
+      ) |>
+      purrr::list_c()
+  ) |>
+  dplyr::filter(n >= 3)
+
+statuts <- liste_sp_pro |>
+  dplyr::pull(code_ref) |>
+  purrr::map(
+    .progress = TRUE,
+    .f = function(x) {
+      taxref4R::get_taxa_status(x) |>
+        dplyr::filter(
+          statusTypeGroup %in% c("Protection", "Liste rouge"),
+          locationName %in% c("France métropolitaine", "Ile-de-France")
+          )
+    }
+  ) |>
+  purrr::list_rbind()
+
+liste_sp_pro <- liste_sp_pro |>
+  dplyr::filter(
+    code_ref %in% (
+      statuts |>
+        dplyr::filter(statusName %in% names(protections)) |>
+        dplyr::pull(referenceId)
+    )
+  )
+
+statuts <- statuts |>
+  dplyr::filter(referenceId %in% liste_sp_pro$code_ref)
+
+habitats <- liste_sp_pro |>
+  dplyr::pull(code_ref) |>
+  purrr::map(
+    .progress = TRUE,
+    .f = function(x) {
+      taxref4R::get_taxa_habitats(x)
+    }
+  ) |>
+  purrr::list_rbind() |>
+  dplyr::rename(code_ref = cd_ref) |>
+  dplyr::filter(Remarque != "Correspondance automatique due à la présence dans un niveau plus fin de la typologie")
+
+sp_pro <- sp_pro |>
+  dplyr::filter(code_ref %in% liste_sp_pro$code_ref) |>
+  dplyr::select(id_sinp, cadre_acquisition, jeu_donnees, observateur, determinateur, code_ref, date, latitude, longitude, etat_biologique, sexe, stade_vie, url_obs, geometry_type) |>
+  dplyr::left_join(
+    liste_sp_pro |>
+      dplyr::select(-n),
+    by = "code_ref"
+    )
+
+couleurs_uicn <- c(
+  EX = "#000000",
+  EW = "#411450",
+  RE = "#5b1764",
+  CR = "#d81e05",
+  EN = "#fc7f3f",
+  VU = "#f9e814",
+  NT = "#cce226",
+  LC = "#60c659",
+  DD = "#d1d1c6",
+  NE = "#ffffff"
+)
+
+couleurs_uicn_font <- c(
+  EX = "white",
+  EW = "white",
+  RE = "white",
+  CR = "white",
+  EN = "black",
+  VU = "black",
+  NT = "black",
+  LC = "black",
+  DD = "black",
+  NE = "black"
+)
+
+limites_idf <- limites_idf |>
+  rmapshaper::ms_simplify()
+
+save(sp_pro, statuts, limites_idf, habitats, file = "fiches/data.rda")
+
+limites_idf |>
+  sf::st_transform(crs = 4326) |>
+  sf::st_write("fiches/limites_idf.geojson", delete_dsn = TRUE)
+
+sp_pro |>
+  dplyr::filter(geometry_type == "POINT") |>
+  dplyr::select(observateur, date, code_ref, stade_vie, url_obs, CLASSE, ORDRE, FAMILLE, ESPECE) |>
+  sf::st_transform(crs = 4326) |>
+  dplyr::group_by(code_ref) |>
+  dplyr::group_split() |>
+  purrr::walk(
+    function(df) {
+      df |>
+        sf::st_write(
+          paste0("fiches/obs_", unique(df$code_ref), ".geojson"),
+          delete_dsn = TRUE
+        )
+    },
+    .progress = TRUE
+  )
+
+purrr::walk(
+  c("logo_inpn.png", "logo_geonatidf.png", "index.qmd"),
+  function(x) {
+    file.copy(
+      from = paste0("templates/", x),
+      to = paste0("fiches/", x),
+      overwrite = TRUE
+    )
+  }
+)
+
+liste_sp_pro |>
+  dplyr::filter(code_ref %in% (
+    statuts |>
+      dplyr::filter(statusName %in% names(protections)) |>
+      dplyr::pull(referenceId)
+  )) |>
+  # dplyr::slice_head(n = 10) |>
+  dplyr::pull(code_ref) |>
+  purrr::walk(
+    function(cd_ref) {
+      brew::brew(
+        file = "templates/template_fiche_espece.qmd",
+        output = paste0("fiches/fiche_", cd_ref, ".qmd")
+      )
+    },
+    .progress = TRUE
+  )
+
+panneau_gauche <- list(
+  list(
+    style = "floating",
+    contents = liste_sp_pro |>
+      dplyr::mutate(
+        GROUPE = dplyr::case_when(
+          is.na(CLASSE) ~ "Reptiles",
+          CLASSE %in% c("Bivalvia", "Gastropoda") ~ "Mollusques",
+          CLASSE %in% c("Malacostraca") ~ "Crustacés",
+          CLASSE %in% c("Insecta") ~ "Insectes",
+          CLASSE %in% c("Petromyzonti", "Actinopterygii") ~ "Poissons",
+          CLASSE %in% c("Amphibia") ~ "Amphibiens",
+          CLASSE %in% c("Aves") ~ "Oiseaux",
+          CLASSE %in% c("Mammalia") ~ "Mammifères"
+        ) |>
+          factor(levels = c(
+            "Mollusques", "Crustacés", "Insectes", "Poissons", "Amphibiens", "Reptiles", "Oiseaux", "Mammifères"
+          ))
+      ) |>
+      dplyr::group_by(GROUPE) |>
+      dplyr::group_split() |>
+      purrr::map(
+        function(df_groupe) {
+          list(
+            section = unique(df_groupe$GROUPE),
+            contents = df_groupe |>
+              dplyr::arrange(ORDRE, FAMILLE) |>
+              dplyr::mutate(FAMILLE = FAMILLE |>
+                              forcats::fct_inorder()) |>
+              dplyr::group_by(FAMILLE) |>
+              dplyr::group_split() |>
+              purrr::map(
+                function(df_famille) {
+                  list(
+                    section = unique(df_famille$FAMILLE),
+                    contents = df_famille |>
+                      dplyr::pull(ESPECE) |>
+                      purrr::map(
+                        function(espece) {
+                          cd_nom <- df_famille |>
+                            dplyr::filter(ESPECE == espece) |>
+                            dplyr::pull(code_ref)
+
+                          list(
+                            text = espece,
+                            href = paste0("fiche_", cd_nom, ".qmd")
+                          )
+                        }
+                      )
+                  )
+
+                }
+              )
+          )
+
+        }
+      )
+  )
+)
+
+quarto_config <- yaml::read_yaml("templates/_quarto.yml")
+quarto_config$website$sidebar <- panneau_gauche
+quarto_config |>
+  yaml::write_yaml("fiches/_quarto.yml")
+readLines("fiches/_quarto.yml") |>
+  stringr::str_replace_all(pattern = "yes", replacement = "true") |>
+  writeLines("fiches/_quarto.yml")
+
+quarto::quarto_render(input = "fiches/")
